bfaed375629530ffb9d4982a1baaacbfa6402ec1
Ulugbek Kahramonov <bek.kahramonov@icloud.com>
2026-02-26 12:27:24 -0500

    Update program to match readme

--- midi-generator.cpp
+++ midi-generator.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 class Note {
     public:
-        int velocity;
         int pitch;
+        int velocity;
         int start;
         int end;
-        Note(int v, int p, int s, int e) {
-            velocity = v;
+        Note(int p, int v, int s, int e) {
             pitch = p;
+            velocity = v;
             start = s;
             end = e;
         }
@@ -22,18 +22,41 @@ class Instrument {
     public: 
         int id;
         vector<Note> notes;
-        int start;
-        int end;
 };
 
-vector<string> get_words(string line);
-void print_instruments(vector<Instrument> instruments);
+class Track {
+    public:
+        string name;
+        vector<Instrument> instruments;
+        int duration;
+};
+
+Track generate_track(string f);
+vector<string> get_words(string l);
+void print_track(Track t);
 
 int main(int argc, char* argv[]) {
 
     string line;
 
-    ifstream f(argv[1]);
+    Track track = generate_track(argv[1]);
+
+    print_track(track);
+
+    return 0;
+}
+
+
+Track generate_track(string filename) {
+    Track track;
+
+    ifstream f(filename);
+    string line;
+
+    getline(f,line);
+    vector<string> words = get_words(line);
+    track.name = words[0];
+    track.duration = stoi(words[1]);
 
     vector<Instrument> instruments;
     Instrument instrument;
@@ -41,13 +64,11 @@ int main(int argc, char* argv[]) {
     while (getline(f, line)) {
         vector<string> words = get_words(line);
         if (words[0][0] == 'e') {
-            instruments.push_back(instrument);
+            track.instruments.push_back(instrument);
             instrument = Instrument();
         }
         else if (words[0][0] == 'i') {
             instrument.id = stoi(words[1]);
-            instrument.start = stoi(words[2]);
-            instrument.end = stoi(words[3]);
         }
         else {  
             Note note = Note(stoi(words[0]), stoi(words[1]), stoi(words[2]), stoi(words[3]));
@@ -55,9 +76,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    print_instruments(instruments);
-
-    return 0;
+    return track;
 }
 
 vector<string> get_words(string line) {
@@ -70,13 +89,16 @@ vector<string> get_words(string line) {
     return words;
 }
 
-void print_instruments(vector<Instrument> instruments) {
-    for (Instrument i : instruments) {
-        cout << "Instrument #" << i.id << " | " << i.start << "-" << i.end << endl;
+void print_track(Track t) {
+    printf("%s | 0:00-%d:%02d\n",
+         t.name.c_str(), t.duration / 60, t.duration % 60);
+
+    for (Instrument i : t.instruments) {
+        printf("  instrument #%d\n", i.id);
         for (Note n : i.notes) {
-            cout << "-- ";
-            cout << n.pitch << " " << n.velocity << " | " << n.start << "-" << n.end << endl;
+            printf("    %d %d %d:%02d-%d:%02d\n", 
+                n.pitch, n.velocity, n.start / 60, n.start % 60, n.end / 60, n.end % 60);
         }
-        cout << endl << endl;
+        cout << endl;
     }
 }
